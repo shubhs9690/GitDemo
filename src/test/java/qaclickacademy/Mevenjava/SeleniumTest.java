1cb60a72d89178eb2b8e0699acc395c8464f10bd
ZMO-WIN-ShubhamG-01 <shubhsgavhane@gmail.com>
2021-12-06 15:15:20 +0530

    My first commit

---
+++ src/test/java/qaclickacademy/Mevenjava/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mevenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowseAutomation()
+	{
+
+		System.out.println("BrowseAutomation");	
+	}
+	@Test
+	public void elementsUi()
+	{
+
+		System.out.println("elementsUi");	
+	}
+
+}
